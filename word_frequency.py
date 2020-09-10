f7832c7ade0e0bb5090b273bd9feccc6798f78e0
tleach01 <taylorleach0@gmail.com>
2020-09-10 08:34:15 -0400

    tried something

--- word_frequency.py
+++ word_frequency.py
@@ -4,10 +4,27 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+text = open('one-today.txt', 'r')
+
+d = dict()
+
+for line in text:
+    line = line.strip()
+    line = line.lower()
+    words = line.split(" ")
+
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+for key in list(d.keys()):
+    print(key, ":", d[key])
+    
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    
 
 
 if __name__ == "__main__":
